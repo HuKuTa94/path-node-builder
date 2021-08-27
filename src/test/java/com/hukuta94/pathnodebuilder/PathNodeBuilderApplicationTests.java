93eb01782474bb14e6304f56d68932b37d3b497d
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-08-28 02:11:54 +0500

    Initial Spring Boot app

---
+++ src/test/java/com/hukuta94/pathnodebuilder/PathNodeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hukuta94.pathnodebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathNodeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
