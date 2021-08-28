b3e943145a7120b0c8953e42e7f0fcd96a9ce4c5
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-08-28 18:16:12 +0500

    Added Vector class

---
+++ src/main/java/com/hukuta94/pathnodebuilder/logic/parser/overwatch/model/Vector.java
@@ -0,0 +1,26 @@
+package com.hukuta94.pathnodebuilder.logic.parser.overwatch.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+/**
+ * Describes vector type of overwatch workshop
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vector
+{
+    private double x;
+    private double y;
+    private double z;
+
+    @Override
+    public String toString()
+    {
+        return "Vector(" + x + ", " + y + ", " + z + ")";
+    }
+}
