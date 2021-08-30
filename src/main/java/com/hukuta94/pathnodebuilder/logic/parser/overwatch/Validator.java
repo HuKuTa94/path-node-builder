f9152eeb21474c1e6c33d4afce524e99ecb2d773
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-08-30 22:42:06 +0500

    Added regex in Validator to detect bad characters in name fields, project switched from CLRF to LR

--- src/main/java/com/hukuta94/pathnodebuilder/logic/parser/overwatch/Validator.java
+++ src/main/java/com/hukuta94/pathnodebuilder/logic/parser/overwatch/Validator.java
@@ -2,29 +2,38 @@ package com.hukuta94.pathnodebuilder.logic.parser.overwatch;
 
 import com.hukuta94.pathnodebuilder.logic.parser.overwatch.model.Constants;
 
+import java.util.regex.Pattern;
+
 public abstract class Validator
 {
+    private static final int INDEX_LOW = 0;
+    private static final int INDEX_HIGH = 127;
+
     public static void validateVariable(Integer index, String variableName)
     {
         validateVariableIndex(index);
-        validateVariableName(variableName);
+        validateNameField(variableName);
     }
 
     private static void validateVariableIndex(Integer index)
     {
-        if (index < 0 || index > 127) {
+        if (index < INDEX_LOW || index > INDEX_HIGH) {
             throw new IllegalArgumentException("Allowed range for index from 0 to 127 but got: " + index);
         }
     }
 
-    private static void validateVariableName(String variableName)
+    private static void validateNameField(String nameField)
     {
-        if (variableName == null) {
-            throw new NullPointerException("Variable name can't be null");
+        if (nameField == null) {
+            throw new NullPointerException("Name can't be null");
         }
 
-        if (variableName.length() == 0 || variableName.length() > Constants.NAME_FIELD_MAX_LENGTH) {
-            throw new IllegalArgumentException("Variable name is empty or too long. Length: " + variableName.length());
+        if (nameField.length() == 0 || nameField.length() > Constants.NAME_FIELD_MAX_LENGTH) {
+            throw new IllegalArgumentException("Name is empty or too long. Length: " + nameField.length());
+        }
+
+        if (!Pattern.matches(Constants.NAME_FIELD_REGEX, nameField)) {
+            throw new IllegalArgumentException("Name has disallowed characters: " + nameField);
         }
     }
 }
--- src/main/java/com/hukuta94/pathnodebuilder/logic/parser/overwatch/model/Constants.java
+++ src/main/java/com/hukuta94/pathnodebuilder/logic/parser/overwatch/model/Constants.java
@@ -3,4 +3,5 @@ package com.hukuta94.pathnodebuilder.logic.parser.overwatch.model;
 public abstract class Constants
 {
     public static final int NAME_FIELD_MAX_LENGTH = 32;
+    public static final String NAME_FIELD_REGEX = "^([a-zA-Z_][a-zA-Z0-9_]+)$";
 }
