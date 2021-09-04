66524110fb8084502490f4c064bc385ebd6814e3
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-09-05 01:47:57 +0500

    Added DistanceMatrixCalculator class. Added tests

---
+++ src/main/java/com/hukuta94/pathnodebuilder/logic/DistanceMatrixCalculator.java
@@ -0,0 +1,130 @@
+package com.hukuta94.pathnodebuilder.logic;
+
+import com.hukuta94.pathnodebuilder.domain.model.Vector;
+import com.hukuta94.pathnodebuilder.domain.util.Tuple;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * Calculator of distance matrix of the unit-edge graph
+ */
+@Component
+public class DistanceMatrixCalculator
+{
+    /**
+     * Creates an instance of default or optimized distance matrix.
+     * default matrix result (greedy algorithm):
+     * {0, 1, 2, 3}
+     * {1, 0, 4, 5}
+     * {2, 4, 0, 6}
+     * {3, 5, 6, 0}
+     *
+     * optimized matrix result (lazy algorithm):
+     * {0, 1, 2, 3}
+     * {0, 4, 5}
+     * {0, 6}
+     *
+     * @param size of the the first array
+     * @param optimized true - fill upper part of the main diagonal only (matrix[x][y]). It can reduce used memory
+     *                  and improve performance. false - fill the full matrix. The lower part of the main diagonal
+     *                  will have inverted values (matrix[y][x])
+     */
+    private int[][] createEmptyDistanceMatrix(int size, boolean optimized)
+    {
+        if (!optimized)
+        {
+            return new int[size][size];
+        }
+
+        int[][] distanceMatrix = new int[size - 1][];
+        int x = 0;
+        for (int y = size; y > 1; y--)
+        {
+            distanceMatrix[x] = new int[y];
+            x++;
+        }
+
+        return distanceMatrix;
+    }
+
+    /**
+     * Calculates distance matrix from output optimized data (position array and connection array)
+     * @param outputOptimizedData optimized data that is ready to calculate the distance matrix
+     * @param optimized true - fill upper part of the left diagonal only (matrix[x][y]). It can reduce used memory
+     *                  and improve performance. false - fill the full matrix. The lower part of the main diagonal
+     *                  will have inverted values (matrix[y][x])
+     * @return the computed distance matrix that is ready to use in mods of Overwatch Workshop based on Path Node Builder
+     */
+    public int[][] calculate(Tuple<Vector[], int[][]> outputOptimizedData, boolean optimized)
+    {
+        Vector[] outputPositions = outputOptimizedData.getObjectA();
+        int[][] outputConnections = outputOptimizedData.getObjectB();
+        int[][] distanceMatrix = createEmptyDistanceMatrix(outputPositions.length, optimized);
+
+        // Calculate all distances for each node
+        for (int startNodeId = 0; startNodeId < distanceMatrix.length; startNodeId++)
+        {
+            Deque<Integer> queue = new ArrayDeque<>();
+            Set<Integer> visitedNodes = new HashSet<>();
+
+            // Add start node id in the queue and mark it as visited
+            queue.addLast(startNodeId);
+            visitedNodes.add(startNodeId);
+
+            // Visit all nodes
+            while (!queue.isEmpty())
+            {
+                // Poll first node from queue
+                Integer currentNodeId = queue.pollFirst();
+
+                // Visit neighbor nodes
+                for (int endNodeId : outputConnections[currentNodeId])
+                {
+                    // Add neighbor node in queue and mark it as visited
+                    if (!visitedNodes.contains(endNodeId))
+                    {
+                        queue.addLast(endNodeId);
+                        visitedNodes.add(endNodeId);
+
+                        // Skip element if it is lower than the main diagonal
+                        if (optimized && endNodeId <= startNodeId) {
+                            continue;
+                        }
+
+                        // Accumulate distance to neighbor
+                        // Default matrix algorithm (gready)
+                        if (!optimized) {
+                            distanceMatrix[startNodeId][endNodeId] = distanceMatrix[startNodeId][currentNodeId] + 1;
+                            continue;
+                        }
+
+                        // Optimized matrix algorithm (lazy)
+                        // End node is lower than the main diagonal
+                        if (startNodeId > endNodeId) {
+                            distanceMatrix[startNodeId][endNodeId - startNodeId] = distanceMatrix[startNodeId][currentNodeId - endNodeId] + 1;
+                        }
+
+                        // End node is higher than the main diagonal
+                        else {
+                            int currentDistance;
+
+                            // Current node has been visited previously (it is lower than the main diagonal)
+                            if (currentNodeId < startNodeId) {
+                                currentDistance = distanceMatrix[currentNodeId][startNodeId - currentNodeId];
+                            }
+                            // Current node has not been visited before (it is higher than the main diagonal)
+                            else {
+                                currentDistance = distanceMatrix[startNodeId][currentNodeId - startNodeId];
+                            }
+
+                            distanceMatrix[startNodeId][endNodeId - startNodeId] = currentDistance + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return distanceMatrix;
+    }
+}
---
+++ src/test/java/com/hukuta94/pathnodebuilder/logic/DistanceMatrixCalculatorTest.java
@@ -0,0 +1,118 @@
+package com.hukuta94.pathnodebuilder.logic;
+
+import com.hukuta94.pathnodebuilder.domain.model.Vector;
+import com.hukuta94.pathnodebuilder.domain.util.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class DistanceMatrixCalculatorTest
+{
+    @Autowired
+    DistanceMatrixCalculator distanceMatrixCalculator;
+
+    private Tuple<Vector[], int[][]> testData;
+
+    private Tuple<Vector[], int[][]> getOptimizedTestData()
+    {
+        if (testData != null) {
+            return testData;
+        }
+
+        Vector[] positions = new Vector[]
+            {
+                new Vector(-16.004, 0.350, -15.965),
+                new Vector(-15.994, 0.350, -0.043),
+                new Vector(-15.981, 0.350, 15.908),
+                new Vector(15.941, 0.350, 16.021),
+                new Vector(15.980, 0.350, -15.878),
+                new Vector(-1.423, 0.350, -16.035),
+                new Vector(-1.225, 0.350, 4.747),
+                new Vector(7.259, 0.350, -0.358)
+            };
+
+        int[][] connections = new int[][]
+            {
+                {5, 1, 6},
+                {0, 2},
+                {3, 1},
+                {2, 6, 4},
+                {5, 3, 7},
+                {4, 0, 7},
+                {0, 3, 7},
+                {6, 5, 4}
+            };
+
+        testData = new Tuple<>(positions, connections);
+        return testData;
+    }
+
+    private void assertDistanceMatrixArrays(int[][] expected, int[][] actual)
+    {
+        // Length of outer array
+        assertEquals(expected.length, actual.length);
+
+        for (int i = 0; i < actual.length; i++)
+        {
+            // Length of inner array
+            assertEquals(expected[i].length, actual[i].length);
+
+            // Values of inner elements
+            for (int j = 0; j < actual[i].length; j++)
+            {
+                assertEquals(expected[i][j], actual[i][j]);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Default distance matrix (greedy algorithm)")
+    void defaultDistanceMatrixTest()
+    {
+        // Given:
+        int[][] expectedDistanceMatrix = new int[][]
+            { // 0  1  2  3  4  5  6  7    y/x (node indexes)
+                {0, 1, 2, 2, 2, 1, 1, 2},  // 0
+                {1, 0, 1, 2, 3, 2, 2, 3},  // 1
+                {2, 1, 0, 1, 2, 3, 2, 3},  // 2
+                {2, 2, 1, 0, 1, 2, 1, 2},  // 3
+                {2, 3, 2, 1, 0, 1, 2, 1},  // 4
+                {1, 2, 3, 2, 1, 0, 2, 1},  // 5
+                {1, 2, 2, 1, 2, 2, 0, 1},  // 6
+                {2, 3, 3, 2, 1, 1, 1, 0},  // 7
+            };
+
+        // When:
+        int[][] actualDistanceMatrix = distanceMatrixCalculator.calculate(getOptimizedTestData(), false);
+
+        // Then:
+        assertDistanceMatrixArrays(expectedDistanceMatrix, actualDistanceMatrix);
+    }
+
+    @Test
+    @DisplayName("Optimized distance matrix (lazy algorithm)")
+    void optimizedDistanceMatrixTest()
+    {
+        // Given:
+        int[][] expectedDistanceMatrix = new int[][]
+            {
+                {0, 1, 2, 2, 2, 1, 1, 2},
+                {0, 1, 2, 3, 2, 2, 3},
+                {0, 1, 2, 3, 2, 3},
+                {0, 1, 2, 1, 2},
+                {0, 1, 2, 1},
+                {0, 2, 1},
+                {0, 1},
+            };
+
+        // When:
+        int[][] actualDistanceMatrix = distanceMatrixCalculator.calculate(getOptimizedTestData(), true);
+
+        // Then:
+        assertDistanceMatrixArrays(expectedDistanceMatrix, actualDistanceMatrix);
+    }
+}
