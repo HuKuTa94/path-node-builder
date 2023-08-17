15d7d2386c7ab5a7415d206471caabec3b9b0ca1
Nikita Koshelev <hukuta94@users.noreply.github.com>
2023-08-17 15:05:25 +0500

    add web controllers

---
+++ src/main/kotlin/com/hukuta94/pathnodebuilder/adapter/web/PresentationController.kt
@@ -0,0 +1,15 @@
+package com.hukuta94.pathnodebuilder.adapter.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/")
+class PresentationController {
+
+    @GetMapping
+    fun indexPage(): String {
+        return "index"
+    }
+}
